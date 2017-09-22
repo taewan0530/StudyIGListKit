2d509fb697337c3a14d413aff73875978f9c758c
wani.kim <wani.kim@kakaobank.com>
2017-09-22 20:32:50 +0900

    init

---
+++ ExampleProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by wani.kim on 2017. 9. 22..
+//  Copyright © 2017년 wani.kim. All rights reserved.
+//
+
+import UIKit
+import IGListKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        IGListCollectionView
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
